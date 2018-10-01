0405100d2218bf59f7519c4d1d6027a8a065fdde
Paulo Rocha <ph.alcantara.rocha@gmail.com>
2018-09-30 21:08:33 -0300

    Making metrics iterations chart larger

--- src/Apps/MetricIterationApp.js
+++ src/Apps/MetricIterationApp.js
@@ -80,13 +80,13 @@ class MetricIterationApp extends Component {
             clusters={this.state.clusters[this.state.n_sim][this.state.itr]}
             title={`Sim: ${this.state.n_sim} Itr: ${this.state.itr}`}
             axis={{ x: { minimum: 0, maximum: 1 }, y: {minimum: 0, maximum:1}}}
-            size={{ heigth: 250, width: 'medium' }} //large, medium, small
+            size={{ heigth: 250, width: 'medium' }} //larger, large, medium, small
           />
         )}
         <MultiMetricChartList 
           met_results={this.state.met_results}
           axis={{ x: 'Iterations', y: 'Values' }}
-          size={{ heigth: 300, width: 'large'}} //large, medium, small
+          size={{ heigth: 300, width: 'larger'}} //larger, large, medium, small
           onClickChart = {values => (
             this.createClusterChart(values)
           )}
